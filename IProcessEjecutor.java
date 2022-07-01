e77ba9c8ccf53e0d41605e004b4ce322ae1e0022
SusanaMA2 <103949963+SusanaMA2@users.noreply.github.com>
2022-07-01 07:06:43 -0500

    Delete IProcessEjecutor.java

--- IProcessEjecutor.java
+++
@@ -1,6 +0,0 @@
-//para generar excepciones
-public interface IProcessEjecutor {
-    public void ejecuteProcess(int idProceso, String usuario, String contraseña)
-        throws Exception; //si algo ocurre con el metodo me lance o capture(throws)  
-                            //cuando no se genera el metodo,tambien para controlar la ejcucion de los procesos
-}
